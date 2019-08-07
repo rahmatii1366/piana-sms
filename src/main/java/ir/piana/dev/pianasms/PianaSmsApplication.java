d524437ef092f7325eab2b73000c06889202fbf9
m.rahmati <rahmatii1366@gmail.com>
2019-08-07 09:23:37 +0430

    initial commit

---
+++ src/main/java/ir/piana/dev/pianasms/PianaSmsApplication.java
@@ -0,0 +1,13 @@
+package ir.piana.dev.pianasms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PianaSmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PianaSmsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ir/piana/dev/pianasms/core/service/KaveNegarService.java
@@ -0,0 +1,22 @@
+package ir.piana.dev.pianasms.core.service;
+
+import com.kavenegar.sdk.KavenegarApi;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author Mohamad Rahmati (rahmatii1366@gmail.com)
+ * Date: 8/7/2019 9:12 AM
+ **/
+@Service
+public class KaveNegarService {
+    private static final String API_KEY = "6B6773663258696B304F65576F4433516739573856513D3D";
+
+    private KavenegarApi api;
+
+    @PostConstruct
+    public void init() {
+        KavenegarApi  api= new KavenegarApi(API_KEY);
+    }
+}
